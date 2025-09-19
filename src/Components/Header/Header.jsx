e43c669c070f98ff38c175902ae87cc00911d5bb
Sarthak Sain <Sarthaksain08@gmail.com>
2025-09-19 11:05:03 +0530

    height

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -43,7 +43,7 @@ function Header() {
             <nav className='flex h-18'>
                 <div className=' mr-4'>
                     <Link to='/'>
-                    <Logo class="w-12"/>
+                    <Logo class="w-5"/>
 
                     </Link>
                 </div>
